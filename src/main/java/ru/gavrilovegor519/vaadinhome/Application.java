b10d51f30d720dbca8849b1ab31adfcfb15cee42
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-02-14 21:30:48 +0300

    Enable favicon

--- src/main/java/ru/gavrilovegor519/vaadinhome/Application.java
+++ src/main/java/ru/gavrilovegor519/vaadinhome/Application.java
@@ -1,6 +1,7 @@
 package ru.gavrilovegor519.vaadinhome;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,5 +19,10 @@ public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void configurePage(AppShellSettings settings) {
+        settings.addFavIcon("icon", "icons/icon.png", "500x500");
     }
 }
