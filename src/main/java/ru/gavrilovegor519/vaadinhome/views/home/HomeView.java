1c3db6976afe6293329e8aef3d227c7e0bc5aa76
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-02-20 23:44:34 +0300

    Some optimizations of code

--- src/main/java/ru/gavrilovegor519/vaadinhome/views/home/HomeView.java
+++ src/main/java/ru/gavrilovegor519/vaadinhome/views/home/HomeView.java
@@ -9,7 +9,6 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @PageTitle("Егор Гаврилов - хоумпейдж")
@@ -21,11 +20,6 @@ public class HomeView extends Composite<VerticalLayout> {
     private final UnorderedList skills = new UnorderedList();
     private final H3 linksPartHeading = new H3();
     private final UnorderedList links = new UnorderedList();
-
-    private final Paragraph fidoNetPart = new Paragraph("""
-                                                   Также я являюсь самым молодым сисопом в
-                                                   Фидонете на сегодняшний день - 2:5015/519.
-                                                   """);
 
     private final VerticalLayout content = new VerticalLayout();
     private final VerticalLayout content2 = new VerticalLayout();
@@ -48,42 +42,40 @@ public class HomeView extends Composite<VerticalLayout> {
         profession.setWidth("max-content");
     }
 
-    private List<String> getSkills() {
-        List<String> skills = new ArrayList<>();
-        skills.add("Java 17");
-        skills.add("Spring 5/6 (Web, Data JPA, Security)");
-        skills.add("JPA, SQL");
-        skills.add("Linux, Docker");
-        return skills;
-    }
-
-    private List<Anchor> getLinks() {
-        List<Anchor> links = new ArrayList<>();
-        links.add(new Anchor("https://github.com/gavrilovegor519", "GitHub"));
-        links.add(new Anchor("https://www.youtube.com/@gavrilovegor519", "YouTube"));
-        links.add(new Anchor("https://habr.com/ru/users/gavrilovegor519/", "Habr"));
-        links.add(new Anchor("https://t.me/gavrilovegor519", "Telegram"));
-        return links;
-    }
-
     private void setSkillsPartSettings() {
         skillsPartHeading.setText("Мой стек:");
 
-        for (String skill : getSkills()) {
+        List<String> skillsList = List.of("Java 17", "Spring 5/6 (Web, Data JPA, Security)", "JPA, SQL", "Linux, Docker");
+
+        skillsList.forEach(skill -> {
             ListItem item = new ListItem(skill);
             item.setWhiteSpace(HasText.WhiteSpace.PRE_WRAP);
             skills.add(item);
-        }
+        });
     }
 
     private void setLinksPartSettings() {
         linksPartHeading.setText("Ссылки:");
 
-        for (Anchor link : getLinks()) {
+        List<Anchor> linksList = List.of(
+                new Anchor("https://github.com/gavrilovegor519", "GitHub"),
+                new Anchor("https://www.youtube.com/@gavrilovegor519", "YouTube"),
+                new Anchor("https://habr.com/ru/users/gavrilovegor519/", "Habr"),
+                new Anchor("https://t.me/gavrilovegor519", "Telegram")
+        );
+
+        linksList.forEach(link -> {
             ListItem item = new ListItem(link);
             item.setWhiteSpace(HasText.WhiteSpace.PRE_WRAP);
             links.add(item);
-        }
+        });
+    }
+
+    private Paragraph getAdditionalInfo() {
+        return new Paragraph("""
+                                                   Также я являюсь самым молодым сисопом в
+                                                   Фидонете на сегодняшний день - 2:5015/519.
+                                                   """);
     }
 
     private void setupLayout() {
@@ -95,7 +87,7 @@ public class HomeView extends Composite<VerticalLayout> {
         content.setMaxWidth("800px");
         content.setAlignItems(Alignment.CENTER);
 
-        content2.add(skillsPartHeading, skills, linksPartHeading, links, fidoNetPart);
+        content2.add(skillsPartHeading, skills, linksPartHeading, links, getAdditionalInfo());
         content.add(myName, profession, content2);
         getContent().add(content);
     }
